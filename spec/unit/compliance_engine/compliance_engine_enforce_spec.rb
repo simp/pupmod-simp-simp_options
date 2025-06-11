297d6e4f4b16b5745d62e31f5261763ddc894060
Chris Tessmer <chris.tessmer@onyxpoint.com>
2025-06-10 21:10:24 -0400

    Fix rubocop complaints

--- spec/unit/compliance_engine/compliance_engine_enforce_spec.rb
+++ spec/unit/compliance_engine/compliance_engine_enforce_spec.rb
@@ -40,6 +40,7 @@ describe 'compliance_markup', type: :class do
                              target_compliance_profile: target_profile
                            })
           end
+          # rubocop:disable RSpec/InstanceVariable
           let(:compliance_report) do
             @compliance_report ||= JSON.parse(
                 catalogue.resource("File[#{facts[:puppet_vardir]}/compliance_report.json]")[:content],
@@ -52,6 +53,7 @@ describe 'compliance_markup', type: :class do
 
             @compliance_profile_data
           end
+          # rubocop:enable RSpec/InstanceVariable
 
           let(:pre_condition) do
             %(
