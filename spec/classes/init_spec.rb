a3e2d9d8053c2589ff51247f7ebfd54df3a0f08f
Steven Pritchard <steve@sicura.us>
2025-08-28 03:34:39 -0500

    Clean up for rubocop (#88)
    * Clean up for rubocop
    * Bump simp-rake-helpers and simp-beaker-helpers versions
    
    ---------
    
    Co-authored-by: Chris Tessmer <chris.tessmer@onyxpoint.com>

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-shared_examples_for  'a simp_options class' do
+shared_examples_for 'a simp_options class' do
   it { is_expected.to compile.with_all_deps }
   it { is_expected.to contain_class('simp_options::dns') }
   it { is_expected.to contain_class('simp_options::ntpd') }
@@ -14,53 +14,58 @@ end
 describe 'simp_options' do
   context 'supported operating systems' do
     on_supported_os.each do |os, os_facts|
-
       context "on #{os}" do
-        let(:facts){ os_facts }
+        let(:facts) { os_facts }
 
         context 'default parameters' do
-          it_should_behave_like "a simp_options class"
+          it_behaves_like 'a simp_options class'
         end
 
         context 'with ldap enabled' do
           context 'with the simp_options::puppet settings set' do
             let(:hieradata) { "#{class_name}_with_ldap" }
-            let(:params){{ :ldap => true }}
-            it_should_behave_like "a simp_options class"
+            let(:params) { { ldap: true } }
+
+            it_behaves_like 'a simp_options class'
             it { is_expected.to contain_class('simp_options::ldap') }
           end
 
           context 'without the simp_options::puppet settings set' do
             let(:hieradata) { "#{class_name}_with_ldap_missing_puppet" }
-            let(:params){{ :ldap  => true }}
-            it { expect{is_expected.to compile.with_all_deps}.to raise_error(/parameter .+ expects/) }
+            let(:params) { { ldap: true } }
+
+            it { expect { is_expected.to compile.with_all_deps }.to raise_error(%r{parameter .+ expects}) }
           end
         end
 
         context 'with pki=true' do
           let(:hieradata) { "#{class_name}_with_pki_true" }
-          let(:params){{ :pki => true }}
-          it_should_behave_like "a simp_options class"
+          let(:params) { { pki: true } }
+
+          it_behaves_like 'a simp_options class'
           it { is_expected.to contain_class('simp_options::pki') }
         end
 
         context 'with pki=simp' do
           let(:hieradata) { "#{class_name}_with_pki_simp" }
-          let(:params){{ :pki => true }}
-          it_should_behave_like "a simp_options class"
+          let(:params) { { pki: true } }
+
+          it_behaves_like 'a simp_options class'
           it { is_expected.to contain_class('simp_options::pki') }
         end
 
         context 'with syslog enabled' do
           let(:hieradata) { class_name }
-          let(:params){{ :syslog => true }}
-          it_should_behave_like "a simp_options class"
+          let(:params) { { syslog: true } }
+
+          it_behaves_like 'a simp_options class'
           it { is_expected.to contain_class('simp_options::syslog') }
         end
 
         context 'invalid trusted_nets' do
           let(:hieradata) { class_name }
-          let(:params){{ :trusted_nets => ['1.2.3.400'] }}
+          let(:params) { { trusted_nets: ['1.2.3.400'] } }
+
           it { is_expected.not_to compile.with_all_deps }
         end
       end
--- spec/classes/syslog_spec.rb
+++ spec/classes/syslog_spec.rb
@@ -3,28 +3,32 @@ require 'spec_helper'
 describe 'simp_options' do
   context 'supported operating systems' do
     on_supported_os.each do |os, facts|
-
       context "on #{os}" do
-        let(:facts){ facts }
+        let(:facts) { facts }
 
         context 'default parameters for simp_options::syslog' do
-          let(:hieradata) { 'simp_options'}
-          let(:params){{ :syslog => true }}
+          let(:hieradata) { 'simp_options' }
+          let(:params) { { syslog: true } }
+
           it { is_expected.to compile.with_all_deps }
-          it { is_expected.to contain_class('simp_options::syslog').with(
-            :log_servers => [],
-            :failover_log_servers => []
-          ) }
+          it {
+            is_expected.to contain_class('simp_options::syslog').with(
+            log_servers: [],
+            failover_log_servers: [],
+          )
+          }
         end
 
         context 'invalid simp_options::syslog::log_servers' do
-          let(:hieradata) { 'simp_options_with_invalid_syslog_log_servers'}
-          it { is_expected.not_to compile.with_all_deps}
+          let(:hieradata) { 'simp_options_with_invalid_syslog_log_servers' }
+
+          it { is_expected.not_to compile.with_all_deps }
         end
 
         context 'invalid simp_options::syslog::failover_log_servers' do
-          let(:hieradata) { 'simp_options_with_invalid_syslog_failover_log_servers'}
-          it { is_expected.not_to compile.with_all_deps}
+          let(:hieradata) { 'simp_options_with_invalid_syslog_failover_log_servers' }
+
+          it { is_expected.not_to compile.with_all_deps }
         end
       end
     end
